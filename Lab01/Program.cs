16a1642b4410fc45aa7d0e5d7cccf4a0dfc03e48
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2024-02-27 11:25:47 +0100

    lab1

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var s1 = Console.ReadLine();
+        var s2 = Console.ReadLine();
+
+        if (int.TryParse(s1, out var a) && int.TryParse(s2, out var b))
+        {
+            Console.WriteLine(a + b);
+        }
+        else
+        {
+            Console.WriteLine("Invalid number format");
+        }
+    }
+
+       static void Main2(string[] args)
+       {
+           if (args.Length < 2)
+           {
+               Console.WriteLine("Invalid number of parameters");
+               return;
+           }
+
+           if (int.TryParse(args[0], out var a) && int.TryParse(args[1], out var b))
+           {
+               Console.WriteLine(a + b);
+           }
+           else
+           {
+               Console.WriteLine("Invalid number format");
+           }
+       }
+}
